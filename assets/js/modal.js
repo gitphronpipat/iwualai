57ac732364de64e4cefb95ada6a032929c751538
gitphronpipat <phronpipat.phromsapsak@gmail.com>
2026-05-25 16:47:54 +0700

    remove assets from git

--- assets/js/modal.js
+++
@@ -1,6 +0,0 @@
-// del
-$("#modalDel").on("show.bs.modal", function (event) {
-	var url = $(event.relatedTarget).data("url");
-	$("#del_url").attr("action", url);
-	$("#del_id").val($(event.relatedTarget).data("id"));
-});
